9b0721fb223556e10ff22b09c8d4b3222dde511f
Valerie Lobas <vallobas@gmail.com>
2020-05-09 00:09:36 -0400

    updated html code

--- app.py
+++ app.py
@@ -30,12 +30,10 @@ Base.prepare(db.engine, reflect=True)
 Samples_Metadata = Base.classes.sample_metadata
 Samples = Base.classes.samples
 
-
 @app.route("/")
 def index():
     """Return the homepage."""
     return render_template("index.html")
-
 
 @app.route("/names")
 def names():
@@ -47,7 +45,6 @@ def names():
 
     # Return a list of the column names (sample names)
     return jsonify(list(df.columns)[2:])
-
 
 @app.route("/metadata/<sample>")
 def sample_metadata(sample):
@@ -78,7 +75,6 @@ def sample_metadata(sample):
     print(sample_metadata)
     return jsonify(sample_metadata)
 
-
 @app.route("/samples/<sample>")
 def samples(sample):
     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
@@ -99,7 +95,6 @@ def samples(sample):
         "otu_labels": sample_data.otu_label.tolist(),
     }
     return jsonify(data)
-
 
 if __name__ == "__main__":
     app.run()
--- plots.js
+++ plots.js
@@ -1,4 +1,5 @@
-var url = `/samples/${sample}`;
+// Pull data from json file via Flask website
+var url = "/samples/<sample>";
 d3.json(url).then(function(data) {
     var values = data.sample_values.slice(0,10);
     var labels = data.otu_ids.slice(0, 10);
@@ -7,17 +8,48 @@ d3.json(url).then(function(data) {
     console.log("Values: ",values);
 });
 
-// Trace1 for bar chart
-var trace1 = {
-    x: labels,
-    y: values,
-    text: hovertext,
-    name: "Bar Chart",
-    type: "bar"
-  };
+// Show default bar chart
+function init() {
+  var trace1 = {
+      x: labels,
+      y: values,
+      text: hovertext,
+      name: "Bar Chart",
+      type: "bar"
+    };
 
-// Set trace for plot
-var data = [trace1, trace2];
+  // Render the plot to the div tag with id "barchart"
+  Plotly.newPlot("barchart", trace1);
+}
 
-// Render the plot to the div tag with id "barchart"
-Plotly.newPlot("barchart", data);
+// Dropdown Code
+// On change to the DOM, call getData()
+d3.selectAll("#selDataset").on("change", getData);
+
+// Function called by DOM changes
+function getData() {
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  var dataset = dropdownMenu.property("value");
+  // Initialize an empty array for the country's data
+  var data = [];
+
+  if (dataset == 'us') {
+      data = us;
+  }
+  else if (dataset == 'uk') {
+      data = uk;
+  }
+  else if (dataset == 'canada') {
+      data = canada;
+  }
+  // Call function to update the chart
+  updatePlotly(data);
+}
+
+// Update the restyled plot's values
+function updatePlotly(newdata) {
+  Plotly.restyle("pie", "values", [newdata]);
+}
+
+init();
