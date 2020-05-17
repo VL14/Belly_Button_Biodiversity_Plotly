2c856fd8f454a04375dbf8d8089d0b3f5da8a4e0
Valerie Lobas <vallobas@gmail.com>
2020-05-17 12:04:18 -0400

    all charts are functioning

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# biodiversity
+**Belly Button Biodiversity Interactive Dashboard**
+In this activity, I built an interactive dashboard using the Belly Button biodiversity dataset (http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that that can be found in human belly buttons.
+
+The dataset reveals that a small handful of microbial species (also called Operational Taxonomic Units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+Using the JSON file provided, Javascript, D3, and Plotly, the following were created:
+
+1. A dropdown menu with a list of test subject ID numbers to choose from. These numbers were populated from the JSON file using D3 and Javascript.
+
+2. A horizontal bar chart to display the top 10 OTUs found in the chosen individual.
+
+3. A bubble chart that displays the OTUs for each individual.
+
+4. A table with the individual's demographic information.
+
+The charts and table are updated each time a new ID is selected from the dropdown menu.
--- static/js/plots.js
+++ static/js/plots.js
@@ -1,68 +1,122 @@
-// Pull values from data file for default chart
-d3.json("/samples.json").then(function(data) {
-  var values = data.samples[0].sample_values;
-  var labels = data.samples[0].otu_ids;
-  var hovertext = data.samples[0].otu_labels;
-  //console.log(`Sample values: ${values}`);
-  //console.log(`OTU IDs: ${labels}`);
-  //console.log(`OTU labels: ${hovertext}`);
+// FUNCTION TO CREATE CHARTS
+function makeCharts(index) {
+  d3.json("samples.json").then(function(data) {
+    let sample_values = data.samples[index].sample_values;
+    let otu_ids_raw = data.samples[index].otu_ids;
+    let otu_labels = data.samples[index].otu_labels;
 
-  // Slice values to get first 10 for chart
-  var x = values.slice(0,10);
-  var y = labels.slice(0,10);
-  var t = hovertext.slice(0,10);
-  console.log(x);
-  
-  // Create default bar chart
-  var trace1 = {
-    x: x,
-    y: y,
-    text: t,
-    name: "Bar Chart",
-    type: "bar"
-  };
+    // BAR CHART
+    // Set prefix for otu_id values
+    var otu_ids = otu_ids_raw.map(id => 'OTU ' + id);
 
-  var layout = {
-    title: "Bar Chart",
-    xaxis: {
-      showgrid: true
-    },
-    plot_bgcolor: "#c7c7c7"
-  };
+    // Slice values to get first 10 for chart
+    var x = sample_values.slice(0,10);
+    var y = otu_ids.slice(0,10);
+    var t = otu_labels.slice(0,10);
+    
+    // Create default bar chart
+    var trace1 = [{
+      x: x,
+      y: y,
+      text: t,
+      type: "bar",
+      orientation: "h"
+    }];
 
-  // Clear any existing data
-  d3.select("#bar").node().value = "";
-  
-  // Render the plot to the div tag with id "bar"
-  Plotly.newPlot("bar", trace1, layout);
+    var layout = {
+      title: "Bar Chart",
+      height: "400px",
+      width: "200px",
+      xaxis: {
+        showgrid: true
+      },
+      plot_bgcolor: "#fafafa"
+    };
 
+    // Render the plot to the div tag with id "bar"
+    Plotly.newPlot("bar", trace1, layout);
+
+    // BUBBLE CHART SETUP
+    var trace2 = [{
+      x: otu_ids_raw,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        size: sample_values
+      }
+    }];
+
+    var layout = {
+      title: 'Bubble Chart',
+      showlegend: false,
+      height: 600,
+      width: 600
+    };
+
+    Plotly.newPlot('bubble', trace2, layout);
+
+    // DEMOGRAPHICS TABLE SETUP
+    var meta_table = d3.select("#sample-metadata");
+
+    // Clear existing data
+    meta_table.html("");
+
+    // Pull data from JSON
+    let metadata1 = data.metadata[index];
+    console.log(metadata1);
+
+    // Loop through metadata array and print key/index values to html
+    Object.entries(metadata1).forEach(([key, value]) => 
+      meta_table.append("h6").text(`${key}: ${value}`));
+
+    // Prevent the page from refreshing
+    // d3.event.preventDefault();
+  });
+}
+
+// Call function to create default bar chart
+let defaultIndex = 0;
+makeCharts(defaultIndex);
+
+// DROPDOWN MENU SETUP
+// Select the dropdown menu
+var dropdownMenu = d3.select("#selDataset");
+
+ // Get values for dropdown menu
+ d3.json("samples.json").then(function(data) {
+  var selections = data.names;
+  //console.log(`Dropdown selections: ${selections}`);
+
+  // Set dropdown values to html
+  selections.map((sample) => {
+    var s = dropdownMenu.append("option");
+    s.text(sample);
+})
 });
 
-
-//init();
-
-// Dropdown Code
-d3.selectAll("#selDataset").on("change", changeData);
-
-// Function called by DOM changes
-function changeData() {
-  // Select the dropdown menu
-  var dropdownMenu = d3.select("#selDataset");
-
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-
+// CHART UPDATE CODE
+function chartUpdate() {
   // Get the value of the dropdown menu
-  var selection = dropdownMenu.property("value");
+  var idSelection = dropdownMenu.property("value");
+  console.log(idSelection);
 
-  // Get values for dropdown menu
   d3.json("samples.json").then(function(data) {
     var selections = data.names;
-    console.log(`Dropdown selections: ${selections}`);
-  
-  // Set dropdown values to html
-  selections.forEach((sample) => {
-    dropdownMenu.append(sample).text(sample).property("value",sample);
-  })
-});
+
+    // Create function to get key for selected value
+    function getKeyByValue(object, value) {
+      return Object.keys(object).find(key => object[key] === value);
+    }
+
+    var newIndex = getKeyByValue(selections, idSelection);
+    
+    console.log(newIndex);
+
+    makeCharts(newIndex);
+  });
 }
+
+// Set event listener for dropdown
+d3.selectAll("#selDataset").on("change", chartUpdate);
